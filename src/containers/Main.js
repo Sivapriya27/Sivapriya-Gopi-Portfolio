a5b1c5f3cbe4755c51d7651a9a75f9b8d00685c0
Sivapriya G <114472285+Sivapriya27@users.noreply.github.com>
2025-09-08 17:04:18 -0500

    hash

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -13,7 +13,7 @@ import Error404 from "../pages/errors/error404/Error";
 export default class Main extends Component {
   render() {
     return (
-      <HashRouter basename="/">
+      <HashRouter basename="/Sivapriya-Gopi-Portfolio">
         {" "}
         {/* <-- changed */}
         <Switch>
